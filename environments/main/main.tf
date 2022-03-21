6a4b8c9f006a024246492a4931b5c12dec2d769b
Roland Urbano <urbano.roland@gmail.com>
2022-03-21 09:17:37 +0000

    fixed how vpc outputs are used for gke

--- environments/main/main.tf
+++ environments/main/main.tf
@@ -15,4 +15,6 @@ module "gke" {
   source  = "../../modules/gke"
   project_id = var.project_id
   region = var.region
+  vpc_name = module.vpc.vpc_name
+  vpc_subnet_name = module.vpc.subnet_name
 }
--- modules/gke/variables.tf
+++ modules/gke/variables.tf
@@ -1,2 +1,4 @@
 variable "project_id" {}
 variable "region" {}
+variable "vpc_name" {}
+variable "vpc_subnet_name" {}
