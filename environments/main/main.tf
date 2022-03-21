1b1212a91282bd16da0b956644e10370db60fcfc
Roland Urbano <urbano.roland@gmail.com>
2022-03-21 08:57:06 +0000

    Add gke resources

--- environments/main/main.tf
+++ environments/main/main.tf
@@ -1,6 +1,18 @@
 # Google Infrastructure configuration
 
 provider "google" {
-  project = "gke-first-steps"
+  project = var.project_id
+  region  = var.region
 }
 
+module "vpc" {
+  source  = "../../modules/vpc"
+  project_id = var.project_id
+  region = var.region
+}
+
+module "gke" {
+  source  = "../../modules/gke"
+  project_id = var.project_id
+  region = var.region
+}
